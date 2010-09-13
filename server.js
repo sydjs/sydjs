f9cd4aef4c9e9a198f0ee2cfef22aa964dc7d23a
Lachlan Hardy <lachlan@lachstock.com.au>
2010-09-13 10:06:53 +1000

    moving to Dmitry's code

--- server.js
+++ server.js
@@ -1,19 +1,131 @@
-// Example Express App on Heroku - Copyright Corey Donohoe <atmos@atmos.org> (MIT Licensed)
+var fs = require("fs"),
+    path = require("path"),
+    // Buffer = require("buffer").Buffer,
+    // p404 = new Buffer('<!doctype html><html lang=en><meta charset=utf-8><title>Not Found</title><style>body{background:#22252a;color:#fff;font:300 100.01% "Helvetica Neue",Helvetica,"Arial Unicode MS",Arial,sans-serif;}h1{font-weight:300;text-align:center;padding:5em;color:#ccf;}</style><body><h1>404<br>Page not found'),
+    p404 = '<!doctype html><html lang=en><meta charset=utf-8><title>Not Found</title><style>body{background:#22252a;color:#fff;font:300 100.01% "Helvetica Neue",Helvetica,"Arial Unicode MS",Arial,sans-serif;}h1{font-weight:300;text-align:center;padding:5em;color:#ccf;}</style><body><h1>404<br>Page not found',
+    p500 = '<!doctype html><html lang=en><meta charset=utf-8><title>Internal Error</title><style>body{background:#22252a;color:#fff;font:300 100.01% "Helvetica Neue",Helvetica,"Arial Unicode MS",Arial,sans-serif;}h1{font-weight:300;text-align:center;padding:5em;color:#fcc;}</style><body><h1>500<br>Internal Error<p>',
+    url = require("url");
 
-// add the vendored express to the require path
-require.paths.unshift("vendor/express/lib")
-require.paths.unshift("vendor")
+require("http").createServer(function (req, res) {
+    var name = url.parse(req.url).pathname;
+    name == "/" && (name = "/index.html");
+    var fullPath = path.join(process.cwd(), process.argv[2] || "", name),
+        content;
+    var ext = name.substring(name.lastIndexOf(".") + 1);
+    fs.stat(fullPath, function (err, stats) {
+        if (!err && stats.isFile()) {
+            fs.readFile(fullPath, encoding[ext] || "binary", function (err, data) {
+                if (err) {
+                    res.writeHead(500, {"Content-Type": "text/html"});
+                    // res.end(new Buffer(p500 + err));
+                    res.end(p500 + err);
+                }
+                res.writeHead(200, {"Content-Type": types[ext] || "text/plain"});
+                res.write(data, encoding[ext] || "binary");
+                res.end();
+            });
+        } else {
+            res.writeHead(404, {"Content-Type": "text/html"});
+            res.end(p404);
+        }
+    });
+}).listen(parseInt(process.env.PORT || 8001, 10));
 
-// require express and its plugins
-require("express")
-require("express/plugins")
-
-// require other goodness
-require("connect")
-require("jade")
-
-//require the actual express app
-require ("./lib/app")
-
-// run app
-run(parseInt(process.env.PORT || 8000), null)
+var types = {
+      ai    : "application/postscript",
+      atom  : "application/atom+xml",
+      au    : "audio/basic",
+      avi   : "video/x-msvideo",
+      bmp   : "image/bmp",
+      css   : "text/css",
+      csv   : "text/csv",
+      diff  : "text/x-diff",
+      dmg   : "application/octet-stream",
+      dtd   : "application/xml-dtd",
+      eml   : "message/rfc822",
+      eps   : "application/postscript",
+      gif   : "image/gif",
+      gz    : "application/x-gzip",
+      htm   : "text/html",
+      html  : "text/html",
+      ico   : "image/vnd.microsoft.icon",
+      ics   : "text/calendar",
+      jpeg  : "image/jpeg",
+      jpg   : "image/jpeg",
+      js    : "application/javascript",
+      json  : "application/json",
+      log   : "text/plain",
+      m3u   : "audio/x-mpegurl",
+      m4v   : "video/mp4",
+      manifest: "text/cache-manifest",
+      mid   : "audio/midi",
+      midi  : "audio/midi",
+      mime  : "message/rfc822",
+      mov   : "video/quicktime",
+      mp3   : "audio/mpeg",
+      mp4   : "video/mp4",
+      mp4v  : "video/mp4",
+      mpeg  : "video/mpeg",
+      mpg   : "video/mpeg",
+      ogg   : "application/ogg",
+      pdf   : "application/pdf",
+      pkg   : "application/octet-stream",
+      png   : "image/png",
+      ps    : "application/postscript",
+      psd   : "image/vnd.adobe.photoshop",
+      rar   : "application/x-rar-compressed",
+      rdf   : "application/rdf+xml",
+      rss   : "application/rss+xml",
+      rtf   : "application/rtf",
+      svg   : "image/svg+xml",
+      svgz  : "image/svg+xml",
+      tar   : "application/x-tar",
+      text  : "text/plain",
+      tif   : "image/tiff",
+      tiff  : "image/tiff",
+      ttf   : "application/x-font-ttf",
+      txt   : "text/plain",
+      xhtml : "application/xhtml+xml",
+      zip   : "application/zip"
+  };
+var encoding = {
+    ai    : "binary",
+    atom  : "utf8",
+    au    : "binary",
+    avi   : "binary",
+    bmp   : "binary",
+    css   : "utf8",
+    gif   : "binary",
+    gz    : "binary",
+    htm   : "utf8",
+    html  : "utf8",
+    ico   : "binary",
+    ics   : "binary",
+    jpeg  : "binary",
+    jpg   : "binary",
+    js    : "utf8",
+    json  : "utf8",
+    manifest: "utf8",
+    mov   : "binary",
+    mp3   : "binary",
+    mp4   : "binary",
+    mp4v  : "binary",
+    mpeg  : "binary",
+    mpg   : "binary",
+    ogg   : "binary",
+    pdf   : "binary",
+    pkg   : "binary",
+    png   : "binary",
+    rss   : "utf8",
+    rtf   : "utf8",
+    svg   : "utf8",
+    svgz  : "binary",
+    tar   : "binary",
+    text  : "utf8",
+    tif   : "binary",
+    tiff  : "binary",
+    ttf   : "binary",
+    txt   : "utf8",
+    xhtml : "utf8",
+    zip   : "binary"
+};
